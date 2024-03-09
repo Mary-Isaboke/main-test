d26006eda8a3f69ebeac712515fca7c8fab3e6f2
mary issa <merianmary468@gmail.com>
2024-03-09 21:42:45 +0300

    phase one

---
+++ FYZ/Bikes.py
@@ -0,0 +1,24 @@
+bike ='Suzuki'
+
+if bike == 'Hero':
+    print("bike us Hero")
+
+elif bike == "Suzuki":
+    print("bike is Suzuki")
+
+elif bike == "Yamaha" :
+    print("bike is Yamaha")
+
+else:
+    print("Please choose correct answer")
+
+
+
+
+
+
+
+
+
+
+
---
+++ FYZ/control_F.py
@@ -0,0 +1,13 @@
+marks = 22
+if marks < 30:
+    results = "Failed"
+elif marks > 75:
+    results = "Passed with distinction"
+else:
+    results = "Passed"
+
+print(results)
+
+
+
+   
